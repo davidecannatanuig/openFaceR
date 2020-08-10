e7173cc660bd3aebe04e58b23f2b869c3c11bbc3
Davide Cannata <davide.cannata@cut-e-consulting.com>
2020-08-10 17:13:06 +0100

    major improvements to the mei function

--- funs/MEI.R
+++ funs/MEI.R
@@ -1,28 +1,38 @@
 #' Motion Energy Intensity
-#' @details The function calculate the average energy of motion of 67 the facial points.The frame to frame difference in movement of each facial point is calculated after correcting for the overall poistion of the face. The change of each point is summed for each frame and the resulting vector is averaged.
+#' @details The function calculate the average energy of motion of 67 the facial points.The frame to frame difference in movement of each facial point is calculated after correcting for the average position of the most rigid points (nose bridge and face sides). The change of each point is summed for each frame and the resulting vector is averaged.
 #' @param df: either a dataframe coming from openFace output (for single faces) or an object of class faces (for multiple faces).  
 #' @outpout: a vector with one motion energy per face
 #' @author Davide Cannata - NUIG
 
 
 
-# first, a support function for caculating motion energy for each coordinate
 
-coord_mei <- function(df, shift){ # 
-  sapply(df, function(x) moving_difference(x - shift)) %>% #calculate difference between position in each frame
-    abs() %>% # calculate absolute value of the movement
-    apply(MARGIN = 1, FUN = sum) %>% # sum by row
-    mean() #mean of the resulting values
+### Spport function for calculating average movement of all the contour points ###
+average_mov <- function(df){
+  shift <- apply(df, 1, function(x) mean(x, na.rm = T)) %>%
+    moving_difference()
+  
+  return(shift)
+}
+
+
+
+### Support function for caculating motion energy for each coordinate ####
+coord_mei <- function(df, shift){ 
+  sapply(df, function(x) moving_difference(x) - shift) %>% #calculate difference between position in each frame
+      abs() %>% # calculate absolute value of the movement
+      apply(MARGIN = 1, FUN = sum) # sum by row
+       #mean of the resulting values
 }
 
 
 # CASE A: a single face
-mei_single <- function(df){ # the case with dataframe
+mei_single <- function(df){ 
   
-  
-  x_shift <- df$pose_Tx # the x y and z correction for movement of the head
-  y_shift <- test$pose_Ty  
-  z_shift <- test$pose_Tz
+  # first shifts of position of the whole face are calcultaed by using the less independently "moveable" facial points
+  x_shift <- average_mov(df %>% select(X_0:X_4,X_12:X_16, X_27:X_30))
+  y_shift <- average_mov(df %>% select(Y_0:Y_4,Y_12:Y_16, Y_27:Y_30))
+  z_shift <- average_mov(df %>% select(Z_0:Z_4,Z_12:Z_16, Z_27:Z_30))
   
   ##TODO: should also correct for pitch, yaw and roll?
   
@@ -30,7 +40,7 @@ mei_single <- function(df){ # the case with dataframe
   ymei <- test %>% select(Y_0:Y_67) %>% coord_mei(y_shift)
   zmei <- test %>% select(Z_0:Z_67) %>% coord_mei(z_shift)
   
-  mei <- sum(xmei, ymei, zmei)
+  mei <- statistics((xmei + ymei + zmei))
   
   return(mei)
   
