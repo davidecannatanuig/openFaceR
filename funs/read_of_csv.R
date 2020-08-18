12d37df84107b2ff717496ea977d4a696afb7f32
Davide Cannata <davide.cannata@cut-e-consulting.com>
2020-08-18 14:21:51 +0100

    changed the read csv function so to add file names

--- funs/read_of_csv.R
+++ funs/read_of_csv.R
@@ -1,9 +1,11 @@
 ## import all the extracted file into a list in which each element correspond to a video
 
-read_of_csv <- function(outputfolder){
+read_face_csvs <- function(outputfolder){
   
   my.files <- list.files(path = outputfolder)
-  my.files <- paste(outputfolder, my.files, sep = "")
-  my.df.list <- lapply(my.files, read_csv)
+  my.paths <- paste(outputfolder, my.files, sep = "")
+  my.names <- str_remove(my.files, ".csv")
+  my.df.list <- lapply(my.paths, read_csv)
+  names(my.df.list) = my.names
   return(my.df.list)
 }
