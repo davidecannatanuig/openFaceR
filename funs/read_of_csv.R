f0861b67c37f079f406d88b64c852372ef735c23
Davide Cannata <davide.cannata@cut-e-consulting.com>
2020-08-20 13:48:51 +0100

    improvement of transform function

--- funs/read_of_csv.R
+++ funs/read_of_csv.R
@@ -1,11 +1,17 @@
 ## import all the extracted file into a list in which each element correspond to a video
 
-read_face_csvs <- function(outputfolder){
+#' read face csvs
+#' Function to read all the csvs from one analysis into one face object
+#' @param: output_dir the folder where csvs outputs are stored
+#' @output a face object
+
+read_face_csvs <- function(output_dir){
   
-  my.files <- list.files(path = outputfolder)
-  my.paths <- paste(outputfolder, my.files, sep = "")
+  my.files <- list.files(path = output_dir)
+  my.paths <- paste(output_dir, my.files, sep = "")
   my.names <- str_remove(my.files, ".csv")
   my.df.list <- lapply(my.paths, read_csv)
   names(my.df.list) = my.names
+  #TODO: make it a face object
   return(my.df.list)
 }
